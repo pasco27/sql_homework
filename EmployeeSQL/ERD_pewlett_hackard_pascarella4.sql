60e44b9164c852124961823f74c656268682bcd9
Rob Pascarella <robpascarella@Robs-iMac.local>
2020-10-02 00:54:24 -0500

    only Q5 and Q8 left

--- EmployeeSQL/ERD_pewlett_hackard_pascarella4.sql
+++ EmployeeSQL/ERD_pewlett_hackard_pascarella4.sql
@@ -13,27 +13,14 @@ WHERE hire_date >= '1985-12-31'
 AND hire_date <= '1987-01-01';
 
 --3) List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
--- SELECT "Dept_Manager".dept_no, "Departments".dept_name, "Dept_Emp".emp_no, "Dept_Manager".emp_no, "Employees".last_name, "Employees".first_name
--- FROM "Dept_Manager"
--- JOIN "Dept_Manager".dept_no AS dept_no
--- ON "Dept_Manager".dept_no = "Departments".dept_no
--- JOIN "Dept_Emp".emp_no AS emp_no
--- ON "Dept_Manager".emp_no = "Dept_Emp".emp_no
--- JOIN "Deptar
-
--- ON "Departments".dept_no = "Dept_Manager".dept_no
--- FROM "Departments"
--- JOIN "Dept_Emp"
--- ON "Dept_Emp".dept_no = "Departments".dept_no
-
-
-
--- SELECT customer.first_name, customer.last_name, email.email, phone.phone
--- FROM customer
--- JOIN customer_email AS email
---   ON customer.id = email.customer_id
--- JOIN customer_phone AS phone
---   ON customer.id = phone.customer_id;
+SELECT "Dept_Manager".dept_no, "Departments".dept_name, "Dept_Emp".emp_no, "Dept_Manager".emp_no, "Employees".last_name, "Employees".first_name
+FROM "Employees"
+JOIN "Dept_Manager"
+ON "Dept_Manager".emp_no = "Employees".emp_no
+JOIN "Departments"
+ON "Departments".dept_no = "Dept_Manager".dept_no
+JOIN "Dept_Emp"
+ON "Dept_Emp".dept_no = "Departments".dept_no
 
 --4) List the department of each employee with the following information: employee number, last name, first name, and department name.
 --### We will also need the dept_no to grab the dept_name, if that's cool w/ you...
@@ -44,13 +31,11 @@ ON "Dept_Emp".emp_no = "Employees".emp_no
 JOIN "Departments"
 ON "Departments".dept_no = "Dept_Emp".dept_no;
 
-
 --5) List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 -- SELECT "Employees".first_name, "Employees".last_name, "Employees".sex
 -- FROM "Employees"
 -- WHERE first_name = 'Hercules'
 -- AND last_name = 'B%'
-
 
 --6)List all employees in the Sales department, including their employee number, last name, first name, and department name
 -- Sales == d007,  will also need "Dept_Emp".dept_no
@@ -61,7 +46,6 @@ ON "Dept_Emp".emp_no = "Employees".emp_no
 JOIN "Departments"
 ON "Departments".dept_no = "Dept_Emp".dept_no
 WHERE dept_name = 'Sales'
-
 
 --7)List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
 -- This the same as the last one... 
