57f65d5d7d6be9f420ed943d27ab902b3f41b28c
Rob Pascarella <robpascarella@Robs-iMac.local>
2020-10-04 12:02:35 -0500

    saved with Q7 completed, only Q8 to go.

--- EmployeeSQL/ERD_pewlett_hackard_pascarella4.sql
+++ EmployeeSQL/ERD_pewlett_hackard_pascarella4.sql
@@ -29,7 +29,7 @@ FROM "Employees"
 JOIN "Dept_Emp"
 ON "Dept_Emp".emp_no = "Employees".emp_no
 JOIN "Departments"
-ON "Departments".dept_no = "Dept_Emp".dept_no;
+ON "Departments".dept_no = "Dept_Emp".dept_no
 
 --5) List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 SELECT "Employees".first_name, "Employees".last_name, "Employees".sex
@@ -48,18 +48,18 @@ ON "Departments".dept_no = "Dept_Emp".dept_no
 WHERE dept_name = 'Sales'
 
 --7)List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
--- This the same as the last one... 
--- SELECT "Employees".emp_no, "Employees".last_name, "Employees".first_name, "Departments".dept_name
--- FROM "Employees"
--- JOIN "Dept_Emp"
--- ON "Dept_Emp".emp_no = "Employees".emp_no
--- JOIN "Departments"
--- ON "Departments".dept_no = "Dept_Emp".dept_no
--- WHERE dept_name = 'Sales' 
--- AND dept_name = 'Development'
+--This the same as the last one... 
+SELECT "Employees".emp_no, "Employees".last_name, "Employees".first_name, "Departments".dept_name
+FROM "Employees"
+JOIN "Dept_Emp"
+ON "Dept_Emp".emp_no = "Employees".emp_no
+JOIN "Departments"
+ON "Departments".dept_no = "Dept_Emp".dept_no
+WHERE dept_name = 'Development' 
+OR dept_name = 'Sales'
 
 --8)In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
 SELECT "Employees".last_name
 FROM "Employees"
-COUNT...  DEC 
+COUNT last_name 
 
